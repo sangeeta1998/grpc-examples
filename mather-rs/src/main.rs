8c79b31ed4f0ff63d4bc86c10e2ca12e9be605e8
sangeeta1998 <sangeeta.kakati19@gmail.com>
2024-03-20 09:36:38 +0100

    Initial commit

--- mather-rs/src/main.rs
+++ mather-rs/src/main.rs
@@ -1,5 +1,4 @@
 use tonic::{transport::Server, Request, Response, Status};
-
 use mather_rs::mather_server::{Mather, MatherServer};
 use mather_rs::{AddInputMessage, AddOutputMessage};
 
@@ -8,9 +7,7 @@ pub mod mather_rs {
 }
 
 #[derive(Debug, Default)]
-pub struct MyMather {
-    multiplier: i64,
-}
+pub struct MyMather;
 
 #[tonic::async_trait]
 impl Mather for MyMather {
@@ -20,9 +17,9 @@ impl Mather for MyMather {
     ) -> Result<Response<AddOutputMessage>, Status> {
         let aim = request.into_inner();
 
-        let reply = mather_rs::AddOutputMessage {
-            sum: (aim.first_summand + aim.second_summand) * self.multiplier,
-        };
+        let sum = aim.first_summand + aim.second_summand;
+
+        let reply = AddOutputMessage { sum };
 
         Ok(Response::new(reply))
     }
@@ -35,22 +32,13 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         laddr = "0.0.0.0:5000".to_string();
     }
 
-    let mut multiplier_raw = std::env::var("MULTIPLIER").unwrap_or("1".to_string());
-    if multiplier_raw == "" {
-        multiplier_raw = "1".to_string();
-    }
-
-    let multiplier = multiplier_raw.parse::<i64>().unwrap();
-
-    let mut mather = MyMather::default();
-    mather.multiplier = multiplier;
-
     println!("Listening on {}", laddr);
 
     Server::builder()
-        .add_service(MatherServer::new(mather))
+        .add_service(MatherServer::new(MyMather::default()))
         .serve(laddr.parse()?)
         .await?;
 
     Ok(())
 }
+
