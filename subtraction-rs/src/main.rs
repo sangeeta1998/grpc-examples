9739532d8a714d0c7b71aae23cbf383c34d2c229
sangeeta1998 <sangeeta.kakati19@gmail.com>
2024-03-27 13:27:54 +0100

    Modified Dockerfile, Subtractor/main.rs and proto

--- subtraction-rs/src/main.rs
+++ subtraction-rs/src/main.rs
@@ -1,52 +1,25 @@
-use tonic::{transport::Channel, Request, Response, Status};
-use subtractor_rs::subtractor_client::SubtractorClient; // Import SubtractorClient
-use subtractor_rs::subtractor_server::Subtractor; // Import Subtractor trait
-use subtractor_rs::{SubtractInputMessage, SubtractOutputMessage};
+use tonic::{transport::Server, Request, Response, Status};
+use mather_rs::mather_server::{Mather, MatherServer};
+use mather_rs::{AddInputMessage, AddOutputMessage};
 
-pub mod subtractor_rs {
+pub mod mather_rs {
     tonic::include_proto!("com.pojtinger.felicitas.grpc_examples");
 }
 
-    
-#[derive(Debug)]
-pub struct MySubtractor {
-    addr: String,
-}
-
-impl MySubtractor {
-    pub fn new(addr: &str) -> Self {
-        MySubtractor { addr: addr.to_string() } // Clone addr to take ownership
-    }
-
-    async fn make_subtraction_request(
-        &self,
-        minuend: i64,
-        subtrahend: i64,
-    ) -> Result<i64, Box<dyn std::error::Error>> {
-        let channel = Channel::from_shared(self.addr.clone())?.connect().await?;
-        let mut client = SubtractorClient::new(channel);
-
-        let request = Request::new(SubtractInputMessage {
-            minuend,
-            subtrahend,
-        });
-
-        let response = client.subtract(request).await?;
-        Ok(response.into_inner().difference)
-    }
-}
+#[derive(Debug, Default)]
+pub struct MyMather;
 
 #[tonic::async_trait]
-impl Subtractor for MySubtractor {
-    async fn subtract(
+impl Mather for MyMather {
+    async fn add(
         &self,
-        request: Request<SubtractInputMessage>,
-    ) -> Result<Response<SubtractOutputMessage>, Status> {
-        let sim = request.into_inner();
+        request: Request<AddInputMessage>,
+    ) -> Result<Response<AddOutputMessage>, Status> {
+        let aim = request.into_inner();
 
-        let difference = sim.minuend - sim.subtrahend;
+        let difference = aim.first_summand - aim.second_summand;
 
-        let reply = SubtractOutputMessage { difference };
+        let reply = AddOutputMessage { difference };
 
         Ok(Response::new(reply))
     }
@@ -54,14 +27,18 @@ impl Subtractor for MySubtractor {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let addr = std::env::var("ADDR").unwrap_or("http://localhost:5000".to_string());
+    let mut laddr = std::env::var("LADDR").unwrap_or("0.0.0.0:5001".to_string());
+    if laddr == "" {
+        laddr = "0.0.0.0:5001".to_string();
+    }
 
-    let subtractor = MySubtractor::new(&addr); // Pass a reference to addr instead of ownership
+    println!("Listening on {}", laddr);
 
-    println!("Subtractor listening on {}", addr);
+    Server::builder()
+        .add_service(MatherServer::new(MyMather::default()))
+        .serve(laddr.parse()?)
+        .await?;
 
     Ok(())
 }
-
-
 
